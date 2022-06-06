cec426e5ec453e6978ec8b94796a89d4bce80326
Flavioeds82 <flavio_eduardo@id.uff.br>
2022-06-06 19:06:45 -0300

    ok

--- projeto_olx/src/Components/Header/index.tsx
+++ projeto_olx/src/Components/Header/index.tsx
@@ -1,9 +1,19 @@
 import './style.css';
+import { Link } from 'react-router-dom';
 
 export function Header(){
    return(
-     <div className="header">
-         *** Header  ***
-     </div>
+         <header className='header'>
+            <div className="container">
+                <div className="logo">
+                  <Link to="/" className='link'>
+                    <div className="logo-img"></div>
+                    <div className="logo-text1">O</div>
+                    <div className="logo-text2">L</div>
+                    <div className="logo-text3">X</div>
+                  </Link>
+                </div>
+            </div>
+         </header>
    )
 }
